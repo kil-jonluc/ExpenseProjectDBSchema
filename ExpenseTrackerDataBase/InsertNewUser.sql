b969677e4f344a5650306e9f274ceb975771d9a4
Jonathan Kilpatrick <kilpatrick.jonluc@outlook.com>
2020-01-20 22:13:05 -0500

    Add project files.

---
+++ ExpenseTrackerDataBase/InsertNewUser.sql
@@ -0,0 +1,18 @@
+﻿create PROCEDURE [dbo].[spNewUser_Insert]
+	
+    @FirstName      VARCHAR(50), 
+    @LastName       VARCHAR(50), 
+    @Email          VARCHAR(50), 
+    @UserName       VARCHAR(50), 
+    @Password       VARCHAR(50), 
+    @PhoneNumber    VARCHAR(50), 
+    @SSN            VARCHAR(50)
+
+AS
+
+BEGIN
+
+	INSERT into "User" (FirstName, LastName, Email, UserName, Password, PhoneNumber, SSN)
+    values  (@FirstName, @LastName, @Email, @UserName, @Password, @PhoneNumber, @SSN )
+
+END
---
+++ ExpenseTrackerDataBase/Tables/User.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[UserId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] VARCHAR(50) NULL, 
+    [LastName] VARCHAR(50) NULL, 
+    [Email] VARCHAR(50) NULL, 
+    [UserName] VARCHAR(50) NULL, 
+    [Password] VARCHAR(50) NULL, 
+    [PhoneNumber] VARCHAR(50) NULL, 
+    [SSN] VARCHAR(50) NULL
+)
