1a8192b034803e71b41b07f6a92c058333d54a12
Jonathan Kilpatrick <kilpatrick.jonluc@outlook.com>
2020-07-27 21:36:30 -0400

    fix to post deploy script naming

---
+++ ExpenseTrackerDataBase/PostDeployment/Employers.merge.data.sql
@@ -0,0 +1,22 @@
+﻿SET IDENTITY_INSERT [dbo].[Employer] ON;
+;WITH cte_employers([Id], [CompanyName])
+AS (
+	SELECT * FROM (
+		VALUES
+		(0,'Test Company'),
+		(1,'Kilpatrick Company'),
+		(2,'Townsend Company')
+		) c([Id],[CompanyName]))
+MERGE [dbo].[Employer] as T
+USING cte_employers as S
+ON 1=1 AND T.[Id] = S.[Id]
+WHEN MATCHED THEN
+	UPDATE SET
+	[CompanyName] = S.[CompanyName]
+WHEN NOT MATCHED BY TARGET THEN
+	INSERT([Id], [CompanyName])
+	VALUES(S.[Id], S.[CompanyName])
+WHEN NOT MATCHED BY SOURCE THEN
+	DELETE;
+
+SET IDENTITY_INSERT [dbo].[Employer] OFF;
--- ExpenseTrackerDataBase/PostDeployment/UserRoles.merge.data.sql
+++ ExpenseTrackerDataBase/PostDeployment/UserRoles.merge.data.sql
@@ -1,23 +1,22 @@
-﻿SET IDENTITY_INSERT [dbo].[Employer] ON;
-;WITH cte_employers([Id], [CompanyName])
+﻿SET IDENTITY_INSERT [dbo].[UserRoles] ON;
+;WITH cte_userRoles([Id], [Role], [Description])
 AS (
 	SELECT * FROM (
 		VALUES
-		(0,'Test Company'),
-		(1,'Kilpatrick Company'),
-		(2,'Townsend Company')
-		) c([Id],[CompanyName]))
-MERGE [dbo].[Employer] as T
-USING cte_employers as S
+		(0,0,'Employer'),
+		(1,1,'Employee')
+		) c([Id],[Role], [Description]))
+MERGE [dbo].[UserRoles] as T
+USING cte_userRoles as S
 ON 1=1 AND T.[Id] = S.[Id]
 WHEN MATCHED THEN
 	UPDATE SET
-	[CompanyName] = S.[CompanyName]
+	[Role] = S.[Role],
+	[Description] = S.[Description]
 WHEN NOT MATCHED BY TARGET THEN
-	INSERT([Id], [CompanyName])
-	VALUES(S.[Id], S.[CompanyName])
+	INSERT([Id], [Role], [Description])
+	VALUES(S.[Id], S.[Role], S.[Description])
 WHEN NOT MATCHED BY SOURCE THEN
 	DELETE;
 
-SET IDENTITY_INSERT [dbo].[Employer] OFF;
-
+SET IDENTITY_INSERT [dbo].[UserRoles] OFF;
