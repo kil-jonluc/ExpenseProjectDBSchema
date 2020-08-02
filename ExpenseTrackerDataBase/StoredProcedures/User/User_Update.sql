c762a8ae643c123dd732ce6e4f25c03ddec31ca1
Jonathan Kilpatrick <kilpatrick.jonluc@outlook.com>
2020-08-02 14:57:36 -0400

    adding user update stored procedure -JK

---
+++ ExpenseTrackerDataBase/StoredProcedures/User/User_Update.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Update]
+
+/*This stored proceedure updates user information 
+in the user table
+*/	
+
+    @UserId         int,
+    @FirstName      VARCHAR(50) = " ", 
+    @LastName       VARCHAR(50) = " ", 
+    @Email          VARCHAR(50) = " ", 
+    @UserName       VARCHAR(50) = " ", 
+    @Password       VARCHAR(50) = " ", 
+    @PhoneNumber    VARCHAR(50) = " ", 
+    @SSN            VARCHAR(50) = " ",
+    @RoleId         INT,
+    @EmployerId     INT
+
+AS
+
+BEGIN
+	IF EXISTS (SELECT UserId FROM [User] WHERE UserId = @UserId)
+    UPDATE [User]
+	SET FirstName = @FirstName, LastName = @LastName, Email = @Email, UserName = @UserName, Password = @Password, PhoneNumber = @PhoneNumber, SSN = @SSN, RoleId = @RoleId, EmployerId = @EmployerId
+    WHERE UserId = @UserId
+END
