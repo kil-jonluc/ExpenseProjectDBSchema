9c753166f9966de6c182e38e3f2fd8df2be43751
Jonathan Kilpatrick <kilpatrick.jonluc@outlook.com>
2020-08-11 21:33:15 -0400

    new sproc for updating just status for employer view

---
+++ ExpenseTrackerDataBase/StoredProcedures/Expense/Expense_StatusUpdate.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[Expense_StatusUpdate]
+    @Id         INT,
+    @Status     INT
+AS
+BEGIN
+    IF EXISTS (SELECT Id FROM Expense WHERE Id = @Id)
+        BEGIN
+            UPDATE Expense
+            SET [Status] = @Status
+        END
+END
